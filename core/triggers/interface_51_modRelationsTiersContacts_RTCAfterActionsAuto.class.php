a4c16de00c6d7aaa876b5a306d34ff673443cb4a
William Mead <william.mead@manchenumerique.fr>
2024-02-12 13:53:25 +0100

    Fixed PHP warning

--- core/triggers/interface_51_modRelationsTiersContacts_RTCAfterActionsAuto.class.php
+++ core/triggers/interface_51_modRelationsTiersContacts_RTCAfterActionsAuto.class.php
@@ -5,6 +5,7 @@
  * Copyright (C) 2013		Cedric GROSS			<c.gross@kreiz-it.fr>
  * Copyright (C) 2014		Marcos García		<marcosgdf@gmail.com>
  * Copyright (C) 2015		Bahfir Abbes			<bafbes@gmail.com>
+ * Copyright (C) 2024		William Mead		<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -66,9 +67,11 @@ class InterfaceRTCAfterActionsAuto extends DolibarrTriggers
 
 		$key = 'MAIN_AGENDA_ACTIONAUTO_'.$action;
 
-		if ($object->skipstandardaction) {
-		    unset($object->skipstandardaction);
-            $conf->global->$key = 1;
+		if(property_exists($object, 'skipstandardaction')) {
+			if ($object->skipstandardaction) {
+				unset($object->skipstandardaction);
+				$conf->global->$key = 1;
+			}
 		}
 
         return 0;
